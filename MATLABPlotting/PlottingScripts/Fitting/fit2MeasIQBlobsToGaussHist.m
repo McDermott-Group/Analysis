843a35abbbeb720dd016455d7c8f533352d99f3d
patzinak <pechenezhski@wisc.edu>
2016-11-14 04:44:17 -0600

    Add the plot title, axis labels, etc. as requested by Brad

--- MATLABPlotting/PlottingScripts/Fitting/fit2MeasIQBlobsToGaussHist.m
+++ MATLABPlotting/PlottingScripts/Fitting/fit2MeasIQBlobsToGaussHist.m
@@ -1,15 +1,19 @@
-function [data, maxFidelity, probOne, rotInfo] = fit2MeasIQBlobsToGaussHist(GATE_I, GATE_X, PLOT_HIST)
-%% This function is meant to take data from an identitiy gate and a Pi gate
+function [data, maxFidelity, probOne, rotInfo] = ...
+        fit2MeasIQBlobsToGaussHist(GATE_I, GATE_X, PLOT_HIST)
+% This function is meant to take data from an identitiy gate and a Pi gate
 % and find the centroids of the IQ space blobs, center and rotate them
 % along the I axis and take a line cut through them. Additionally, it will
 % integrate the data and plot the results ERF's of the gaussian
-% distribution
+% distribution.
 
-%% Check to see if gate data was fed into the function or needs to be
+% Number of bins in a histogram.
+Nbins = 250;
+
+% Check to see if gate data was fed into the function or needs to be.
 if ~exist('GATE_I','var')
     % Select files to compute the difference.
     [filenames, pathnames, status] = selectMeasurementDataFile(2,...
-    {'Select the file corresponding to the Idle gate...',...
+    {'Select the file corresponding to the Identity gate...',...
      'Select the file corresponding to the Pi gate...'});
     if ~status
         return
@@ -31,33 +35,33 @@ else
     plotHist = PLOT_HIST;
 end
 
-%% Initialize the data stack
+% Initialize the data stack
 data.gateI.Is = gateI.Is();
 data.gateI.Qs = gateI.Qs();
 data.gateX.Is = gateX.Is();
 data.gateX.Qs = gateX.Qs();
  
-% Combine gateI and gateX I quad data
+% Combine gateI and gateX I quadrature data.
 data.allIs = [data.gateI.Is; data.gateX.Is];
 
-% Combine gateI and gateX Q quad data
+% Combine gateI and gateX Q quadrature data.
 data.allQs = [data.gateI.Qs; data.gateX.Qs];
 
-% combine all I-Q data into one array where the I gate data is 1st half (1:end/2) and X Gate data is the 2nd half
-% of the array (end/2 : end)
+% Combine all I-Q data into one array where the I gate data is the 1st half 
+% (1:end/2) and X Gate data is the 2nd half of the array (end/2 : end).
 data.allIQs = [data.allIs data.allQs];
 
-%% Call centerAndRoate function that finds the IQ blob centers of the combined I and X data sets
+% Call centerAndRoate function that finds the IQ blob centers of
+% the combined I and X data sets
 [data.allIQsRot, rotInfo] = centerAndRotate(data.allIQs);
 
-
 if plotHist
-    
-    figure;
-    histI = histogram(data.allIQsRot(1:end/2,1),250); hold on;
-    histX = histogram(data.allIQsRot(end/2:end,1),250);
+    figure
+    histI = histogram(data.allIQsRot(1:end/2,1), Nbins); 
+    hold on
+    histX = histogram(data.allIQsRot(end/2:end,1), Nbins);
 
-    xValuesI = linspace(histI.BinLimits(1),histI.BinLimits(2),histI.NumBins);
+    xValuesI = linspace(histI.BinLimits(1), histI.BinLimits(2), histI.NumBins);
     fitI = fit(xValuesI', histI.Values', 'gauss1');
     if fitI.b1 > 0
         rotInfo.ZeroStateLoc = 1;
@@ -65,9 +69,8 @@ if plotHist
         rotInfo.ZeroStateLoc = -1;
     end
 
-    xValuesX = linspace(histX.BinLimits(1),histX.BinLimits(2),histX.NumBins);
-    %options = fitoptions('gauss2', 'StartPoint', [100 -fitI.b1 1 100 fitI.b1 1]); 
-fitX = fit(xValuesX', histX.Values', 'gauss1');
+    xValuesX = linspace(histX.BinLimits(1), histX.BinLimits(2), histX.NumBins);
+    fitX = fit(xValuesX', histX.Values', 'gauss1');
 
     h = plot(fitI, '-b');
     set(h, 'LineWidth',2);
@@ -76,33 +79,48 @@ fitX = fit(xValuesX', histX.Values', 'gauss1');
 
     funX = @(x) fitX(x);
     
-    intX = cumtrapz(xValuesX,histX.Values);
-    probOne = integral(funX,-inf,xValuesX(end/2),'ArrayValued',true)/integral(funX,-inf,inf,'ArrayValued',true);
-    intI = cumtrapz(xValuesI,histI.Values);
-fidelity =  @(x) 0.5 * abs((erf((x-fitI.b1)/(fitI.c1))) - (erf((x-fitX.b1)/(fitX.c1))));
-allX = [xValuesX xValuesI];
-for n = 1:length(allX)
-    fids(n) = fidelity(allX(n));
-end
-maxFidelity = max(fids)
+    intX = cumtrapz(xValuesX, histX.Values);
+    probOne = integral(funX, -inf, xValuesX(end/2), 'ArrayValued', true) /...
+        integral(funX, -inf, inf, 'ArrayValued', true);
+    intI = cumtrapz(xValuesI, histI.Values);
+    fidelity =  @(x) 0.5 * abs((erf((x-fitI.b1)/(fitI.c1))) - (erf((x-fitX.b1)/(fitX.c1))));
+    allX = [xValuesX xValuesI];
+    fids = zeros(size(allX));
+    for n = 1:length(allX)
+        fids(n) = fidelity(allX(n));
+    end
+    maxFidelity = max(fids);
     
-% Note: yyaxis not available before 2016a
-    yyaxis right;
-    h = plot(xValuesI,intI/max(intI), '-b');
-    ax = gca;
-    ax.YColor = [0,0,0];
-    set(h, 'LineWidth',2);
-    h = plot(xValuesX,intX/max(intX), '-r');
-    set(h, 'LineWidth',2);
+    ylabel('Counts')
+    % Note: yyaxis not available before 2016a
+    try
+        yyaxis right
+        h = plot(xValuesI, intI/max(intI), '-b');
+        ylabel('Probability')
+        ax = gca;
+        ax.YColor = [0,0,0];
+        set(h, 'LineWidth',2);
+        h = plot(xValuesX, intX/max(intX), '-r');
+        set(h, 'LineWidth',2);
+    catch
+    end
     
+    xunits = getUnits(gateI, 'Is');
+    xlabel(['Generalized Quadrature Coordinate', xunits], 'FontSize', 14)
+    [~, filenameI, extI] = fileparts(gateI.Filename);
+    [~, filenameX, extX] = fileparts(gateX.Filename);
+    title({['Dataset A: ', strrep(filenameI, '_', '\_'), extI,...
+            ' [', gateI.Timestamp, ']'],
+           ['Dataset B: ', strrep(filenameX, '_', '\_'), extX,...
+            ' [', gateX.Timestamp, ']']}, 'FontSize', 10)
+     legend('dataset A', 'dataset B',...
+         'gaussian fit to A', 'gaussian fit to B', 'Location', 'NorthWest')
 else
     
-    [Icounts, Iedges] = histcounts(data.allIQsRot(1:end/2,1),250);
-    % [Xcounts, Xedges] = histcounts(data.allIQsRot(end/2:end,1),250);
+    [Icounts, Iedges] = histcounts(data.allIQsRot(1:end/2,1), Nbins);
     xValuesI = zeros(length(Iedges)-1,1);
     for i=1:length(Iedges)-1
        xValuesI(i) = mean([Iedges(i) Iedges(i+1)]);
-       % xValuesX = mean([Xedges(i) Xedges(i+1)]);
     end
     fitI = fit(xValuesI, Icounts', 'gauss1');
     if fitI.b1 > 0
@@ -123,28 +141,27 @@ function [IQshiftRot, rotInfo] = centerAndRotate(allIQs)
 %  this information and the rotation matrix such that other functions may
 %  perform the exact same rotation and shift in IQ space.
 
-opts = statset('Display','final',...
-               'UseParallel', 0,...
-               'MaxIter', 10000);
+    opts = statset('Display','final',...
+                   'UseParallel', 0,...
+                   'MaxIter', 10000);
 
-[~, C] = kmeans(allIQs, 2, 'Replicates', 1,...
-                                  'Options', opts);
+    [~, C] = kmeans(allIQs, 2, 'Replicates', 1, 'Options', opts);
 
-% Find the center between the 2 blobs
-rotCenterI = mean(C(:,1));
-rotCenterQ = mean(C(:,2));
+    % Find the center between the 2 blobs
+    rotCenterI = mean(C(:,1));
+    rotCenterQ = mean(C(:,2));
 
-IQshift = [allIQs(:,1)- rotCenterI allIQs(:,2) - rotCenterQ];
-centShift = [C(:,1) - rotCenterI C(:,2) - rotCenterQ];
-angle = atan(centShift(1,2)/centShift(1,1));
+    IQshift = [allIQs(:,1)- rotCenterI allIQs(:,2) - rotCenterQ];
+    centShift = [C(:,1) - rotCenterI C(:,2) - rotCenterQ];
+    angle = atan(centShift(1,2) / centShift(1,1));
 
-rotMatrix = [cos(angle) -sin(angle);...
-             sin(angle)  cos(angle)]; 
-IQshiftRot = IQshift * rotMatrix;
+    rotMatrix = [cos(angle) -sin(angle);...
+                 sin(angle)  cos(angle)]; 
 
-rotInfo.RMatrix = rotMatrix;
-rotInfo.ShiftI = rotCenterI;
-rotInfo.ShiftQ = rotCenterQ;
+    IQshiftRot = IQshift * rotMatrix;
 
+    rotInfo.RMatrix = rotMatrix;
+    rotInfo.ShiftI = rotCenterI;
+    rotInfo.ShiftQ = rotCenterQ;
 end
 
--- MATLABPlotting/PlottingScripts/Qubit/plotIQBlobsP1.m
+++ MATLABPlotting/PlottingScripts/Qubit/plotIQBlobsP1.m
@@ -1,10 +1,10 @@
-function [data] = plotIQBlobsP1(CAL_I, CAL_X, DATA, MAKE_PLOTS)
-%% Select the calibration and data files
-%calibration
-if ~exist('CAL_I','var')
+function data = plotIQBlobsP1(CAL_I, CAL_X, DATA, MAKE_PLOTS)
+
+% Select the calibration files.
+if ~exist('CAL_I', 'var')
     [filenames, pathnames, status] = selectMeasurementDataFile(2,...
     {'Select the file corresponding to the Identity calibration...',...
-    'Select the file corresponding to the Pi calibration...'});
+     'Select the file corresponding to the Pi calibration...'});
     
     if ~status
         return
@@ -19,14 +19,14 @@ else
     calX = CAL_X;
 end
 
-%data
-if ~exist('DATA','var')
+% Select the data file.
+if ~exist('DATA', 'var')
     data = loadMeasurementData;
 else
     data = DATA;
 end
 
-if ~exist('MAKE_PLOTS','var')
+if ~exist('MAKE_PLOTS', 'var')
     makePlots = 1;
 else
     makePlots = MAKE_PLOTS;
@@ -35,9 +35,9 @@ end
 % Call external function to find the threshold bisector and offset from the
 % calibration files for |0> and |1>. rotInfo contains a shift and rotation
 % matrix to threshold the data in the same fashion as the calibration files
-[~, ~, ~, rotInfo] = fit2MeasIQBlobsToGaussHist(calI,calX,makePlots);
+[~, ~, ~, rotInfo] = fit2MeasIQBlobsToGaussHist(calI, calX, makePlots);
 
-%% Calculate P1 based on thresholding
+% Calculate P1 based on thresholding.
 % In the case of only one set of Is/Qs (no sweep parameter), only calculate
 % P1 for the one set of reps
 if length(data.Is(1,:)) == 1
@@ -51,18 +51,17 @@ if length(data.Is(1,:)) == 1
         data.stateOne = length(data.allIQsTransformed(data.allIQsTransformed(:,1) > 0,1));
     end
     
-        data.P1 = data.stateOne / (data.stateZero + data.stateOne);
-% Calculate P1 as a function of a single sweep parameter.
-else
+    data.P1 = data.stateOne / (data.stateZero + data.stateOne);
+else % Calculate P1 as a function of a single sweep parameter.
     for n = 1:length(data.Is(:,1))
         data.allIQs(n,:,:) = [data.Is(n,:); data.Qs(n,:)]';
         data.allIQsTransformed(n,:,:) = shiftAndSpin(rotInfo, squeeze(data.allIQs(n,:,:)));
         if rotInfo.ZeroStateLoc > 0
-            data.stateZero = length(data.allIQsTransformed(n,data.allIQsTransformed(n,:,1) > 0,1));
-            data.stateOne = length(data.allIQsTransformed(n,data.allIQsTransformed(n,:,1) < 0,1));
+            data.stateZero = length(data.allIQsTransformed(n, data.allIQsTransformed(n,:,1) > 0, 1));
+            data.stateOne = length(data.allIQsTransformed(n, data.allIQsTransformed(n,:,1) < 0, 1));
         else
-            data.stateZero = length(data.allIQsTransformed(n,data.allIQsTransformed(n,:,1) < 0,1));
-            data.stateOne = length(data.allIQsTransformed(n,data.allIQsTransformed(n,:,1) > 0,1));
+            data.stateZero = length(data.allIQsTransformed(n, data.allIQsTransformed(n,:,1) < 0, 1));
+            data.stateOne = length(data.allIQsTransformed(n, data.allIQsTransformed(n,:,1) > 0, 1));
         end
     
         data.P1(n) = data.stateOne / (data.stateZero + data.stateOne);
@@ -70,12 +69,32 @@ else
 end
 
 if makePlots
-    figure(); plot(data.(data.indep{1}),data.P1);
-    grid on
-    ylabel('P1')
-    set(gca,'FontSize',16)
-    xlabel([data.indep{1} ' (' data.units.(data.indep{1}) ')'])
+    occup_prob = 'Excited_State_Occupation_Probability';
+    data.(occup_prob) = data.P1;
+
+    data.units.(occup_prob) = '';
+    data.rels.(occup_prob) = data.rels.I;
+    data.dep{length(data.dep) + 1} = occup_prob;
+    data.plotting.(occup_prob).full_name =...
+        'Excited State Occupation Probability';
+    [pathname, filename, ext] = fileparts(data.Filename);
+    plts_path = makeDirPlots(pathname);
+    [~, filenameI, extI] = fileparts(calI.Filename);
+    [~, filenameX, extX] = fileparts(calX.Filename);
+    data.plotting.(occup_prob).plot_title =...
+        {['Data: ', strrep(filename, '_', '\_'), ext,...
+        ' [', calI.Timestamp, ']'],...
+        ['Calibration I: ', strrep(filenameI, '_', '\_'), extI,...
+        ' [', calI.Timestamp, ']'],...
+        ['Calibration X: ', strrep(filenameX, '_', '\_'), extX,...
+        ' [', calX.Timestamp, ']']};
+    data.plotting.(occup_prob).plot_filename =...
+            fullfile(plts_path, [filename, '_occup_prob']);
+    
+    plotDataVar(data, occup_prob)
+    ylim([0, 1])
 end
+
 end
 
 
