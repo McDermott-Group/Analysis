c6a86054f0617bc8aa0d1b3fbccde2acc9a6adce
nkna <nkna.iitr@gmail.com>
2018-05-18 16:05:30 -0500

    Updated with more data points for calibration

--- MATLABPlotting/Calibration/ProbeStationDiodeCalibration/RdiodeToRnormal.m
+++ MATLABPlotting/Calibration/ProbeStationDiodeCalibration/RdiodeToRnormal.m
@@ -3,10 +3,14 @@ function [Rn, Ic] = RdiodeToRnormal(Rmeasured)
 % Input is the resistance(in kOhms) with diode mode ON on probe station
 % This function maps from those resistances to the actual resistances
 % measured. And, it outputs actual resistances and corresponding critical
-% currents for Aluminium junctions.
+% currents for Aluminium junctions. 
 
-rDiode = [1,4.975,9.949,14.770,19.320,26.064,31.000,32.81,33.59];
-rNormal = [1,4.984,10.003,14.984,20.03,30.237,50.12,74.851,99.418];
+rDiode1 = [1,4.975,9.949,14.770,32.574,19.320,26.064,31.000,33.59,2.314,7.685,13.686,16.069,22.423,23.304,24.64,30.037,32.668];
+rNormal1 = [1,4.984,10.003,68.397,14.984,20.03,30.237,50.12,99.418,2.316,7.71,13.843,16.367,23.975,25.294,27.439,43.26,69.622];
+%Discarded data points (32.81,74.851)  
+rDiode = sort(rDiode1);
+rNormal = sort(rNormal1);
+
 Rn = spline(rDiode,rNormal,Rmeasured);
 Ic = pi*3.4e-4./(2*Rn*1000)*1e9;
 Rn = {Rn, 'kOhm'};
@@ -27,10 +31,10 @@ valsIc
 
 % Set up fittype and options.
 ft = 'splineinterp';
+ft1 = 'splineinterp';
 
 % Fit model to data.
 [fitresult, ~] = fit( xData, yData, ft, 'Normalize', 'on' );
-
 % Plot fit with data.
 figure( 'Name', 'Fit' );
 h = plot( fitresult, xData, yData );
