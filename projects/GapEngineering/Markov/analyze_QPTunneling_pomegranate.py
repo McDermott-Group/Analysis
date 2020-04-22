b0bf4754e57d48fef25731233ff818c7f0312678
vincentliu66 <vincentchliu@outlook.com>
2020-04-21 20:57:48 -0500

    Starting working with pomegranate package. Have funtionalized all the codes. Next thing is to get more statistics -LIU

--- projects/GapEngineering/Markov/analyze_QPTunneling_pomegranate.py
+++ projects/GapEngineering/Markov/analyze_QPTunneling_pomegranate.py
@@ -3,83 +3,110 @@ from numpy import *
 import string
 import matplotlib.pyplot as plt
 
-n = 100
-Time = arange(0, n, 1)
-Hidden_Signal = [0]*n
-Observed_Signal = [0]*n
 
-# Readout Fidelity
-p_e1 = 0.75
-p_e0 = 1-p_e1
-p_g0 = 0.95
-p_g1 = 1-p_g0
+def generate_hidden_signal(length=5000, charge_burst_time=2500, p_QP=[0.01, 0.01]):
+    """
+    Generate hidden signal from given parameters
+    :param length: the length of the hidden signal, eg. 5000
+    :param charge_burst_time: the time where we have the charge burst, eg. 2456
+    :param p_QP: p_QP = [p_QP_before, p_QP_after], a two element list consists QP tunneling rate before and after the burst
+    :return: the hidden signal [0,1,0,0,1,1,1,...]
+    """
 
-# Measurement setup
-t_QP_before = 10.0     # units ms
-t_QP_after = 10.0     # units ms
-t_meas = 0.1    # units ms
-p_QP_before = 1.0 - exp(-t_meas/t_QP_before)
-p_QP_after = 1.0 - exp(-t_meas/t_QP_after)
+    p_QP_before = p_QP[0]
+    p_QP_after = p_QP[1]
+    hidden_signal = [0] * length
 
-# Numbers for pomegranate which are fixed
-p_e1_VBT = p_e1 + 0.0
-p_e0_VBT = 1-p_e1_VBT
-p_g0_VBT = p_g0 + 0.0
-p_g1_VBT = 1-p_g0_VBT
+    hidden_signal[0] = random.choice([0, 1], p=[0.5, 0.5])  # Initial parity
 
-t_QP_VBT = 10    # units ms
-p_QP_VBT = 1.0 - exp(-t_meas/t_QP_VBT)
+    for i in range(1, charge_burst_time):
+        hidden_signal[i] = random.choice([hidden_signal[i - 1], 1 - hidden_signal[i - 1]],
+                                         p=[1 - p_QP_before, p_QP_before])
+    for i in range(charge_burst_time, length):
+        hidden_signal[i] = random.choice([hidden_signal[i - 1], 1 - hidden_signal[i - 1]],
+                                         p=[1 - p_QP_after, p_QP_after])
 
-# Generating simulation signal
+    return hidden_signal
 
-Hidden_Signal[0] = 1
-for i in range(1, n//2):
-    Hidden_Signal[i] = random.choice([Hidden_Signal[i - 1], 1 - Hidden_Signal[i - 1]], p=[1 - p_QP_before, p_QP_before])
-for i in range(n//2, n):
-    Hidden_Signal[i] = random.choice([Hidden_Signal[i - 1], 1 - Hidden_Signal[i - 1]], p=[1 - p_QP_after, p_QP_after])
 
-for i in range(n):
-    if Hidden_Signal[i] == 0: Observed_Signal[i] = random.choice([0, 1], p=[p_g0, p_g1])
-    if Hidden_Signal[i] == 1: Observed_Signal[i] = random.choice([0, 1], p=[p_e0, p_e1])
+def hidden_to_observed_signal(hidden_signal, readout_fidelity=[0.95, 0.75]):
+    """
+    Convert the hidden signal to the observed signal
+    :param hidden_signal: the hidden signal list [0,1,1,1,0,...]
+    :param readout_fidelity: readout_fidelity = [p_g0, p_e1]
+    :return: observed signal [0,1,1,1,1, ...]
+    """
 
-Observed_Signal_copy = Observed_Signal
-Observed_Signal = str(Observed_Signal).strip('[]')
-Observed_Sequence = ''.join(Observed_Signal)
-Observed_Sequence = Observed_Sequence.replace(',', '')
-Observed_Sequence = Observed_Sequence.translate({ord(c): None for c in string.whitespace})
+    observed_signal = [0] * len(hidden_signal)
 
-d1 = DiscreteDistribution({'0': p_g0_VBT, '1': p_g1_VBT})
-d2 = DiscreteDistribution({'0': p_e0_VBT, '1': p_e1_VBT})
+    # Readout Fidelity
+    p_g0 = readout_fidelity[0]
+    p_e1 = readout_fidelity[1]
+    p_e0 = 1 - p_e1
+    p_g1 = 1 - p_g0
 
-g = State(d1, name ="g")
-e = State(d2, name ="e")
+    for i in range(len(hidden_signal)):
+        if hidden_signal[i] == 0: observed_signal[i] = random.choice([0, 1], p=[p_g0, p_g1])
+        if hidden_signal[i] == 1: observed_signal[i] = random.choice([0, 1], p=[p_e0, p_e1])
 
-model = HiddenMarkovModel('example')
-model.add_states([g, e])
+    return observed_signal
 
-model.add_transition(model.start, g, 0.5)
-model.add_transition(model.start, e, 0.5)
-model.add_transition(g, g, 1-p_QP_VBT)
-model.add_transition(g, e, p_QP_VBT)
-model.add_transition(e, e, 1-p_QP_VBT)
-model.add_transition(e, g, p_QP_VBT)
-model.add_transition(e, model.end, 0.5)
-model.bake()
 
-Recovered_Signal = ", ".join(state.name for i, state in model.viterbi(Observed_Sequence)[1])
+def observed_to_recovered_signal(observed_signal, readout_fidelity=[0.95, 0.75], p_QP=0.01):
+    """
 
-# Data post-processing
-Recovered_Signal = Recovered_Signal.split(',')
-Recovered_Signal = Recovered_Signal[1:-1]
-for i in range(len(Recovered_Signal)):
-    if(Recovered_Signal[i] == ' e'):
-        Recovered_Signal[i] = 1
-    if(Recovered_Signal[i] == ' g'):
-        Recovered_Signal[i] = 0
+    :param observed_signal: observed signal for parity from given readout fidelity
+    :param readout_fidelity: the given readout fidelity will be used as emission probability for Viberti Algorithm
+    :param p_QP: QP tunneling rate
+    :return: recovered signal
+    """
 
-Hidden_Signal = asarray(Hidden_Signal)
-Observed_Signal = asarray(Observed_Signal)
-Recovered_Signal = asarray(Recovered_Signal)
+    p_g0 = readout_fidelity[0]
+    p_e1 = readout_fidelity[1]
+    p_e1_VBT = p_e1 + 0.0
+    p_e0_VBT = 1 - p_e1_VBT
+    p_g0_VBT = p_g0 + 0.0
+    p_g1_VBT = 1 - p_g0_VBT
+
+    p_QP_VBT = p_QP
+
+    observed_signal = str(observed_signal).strip('[]')
+    observed_sequence = ''.join(observed_signal)
+    observed_sequence = observed_sequence.replace(',', '')
+    observed_sequence = observed_sequence.translate({ord(c): None for c in string.whitespace})
+
+    d1 = DiscreteDistribution({'0': p_g0_VBT, '1': p_g1_VBT})
+    d2 = DiscreteDistribution({'0': p_e0_VBT, '1': p_e1_VBT})
+
+    g = State(d1, name="g")
+    e = State(d2, name="e")
+
+    model = HiddenMarkovModel('example')
+    model.add_states([g, e])
+
+    model.add_transition(model.start, g, 0.5)
+    model.add_transition(model.start, e, 0.5)
+    model.add_transition(g, g, 1 - p_QP_VBT)
+    model.add_transition(g, e, p_QP_VBT)
+    model.add_transition(e, e, 1 - p_QP_VBT)
+    model.add_transition(e, g, p_QP_VBT)
+    model.add_transition(e, model.end, 0.5)
+    model.bake()
+
+    recovered_signal = ", ".join(state.name for i, state in model.viterbi(observed_sequence)[1])
+
+    # Data post-processing
+    recovered_signal = recovered_signal.split(',')
+    recovered_signal = recovered_signal[1:-1]
+    for i in range(len(recovered_signal)):
+        if recovered_signal[i] == ' e':
+            recovered_signal[i] = 1
+        if recovered_signal[i] == ' g':
+            recovered_signal[i] = 0
+
+    recovered_signal = asarray(recovered_signal)
+    return recovered_signal
+
 
 def transitions_count(signal):
     """
@@ -88,14 +115,41 @@ def transitions_count(signal):
     :return: the number of 0->1, 1->0 counts
     """
     transitions = 0
-    for i in range(len(signal)-1):
-        if signal[i] != signal[i+1]:
+    for i in range(len(signal) - 1):
+        if signal[i] != signal[i + 1]:
             transitions += 1
     return transitions
 
-fig = plt.figure(figsize=(12, 6))
-plt.plot(Time, Hidden_Signal + 1.5, 'o-', label=r"{} Hidden Transitions".format(transitions_count(Hidden_Signal)))
-plt.plot(Time, Recovered_Signal, 'o-', label=r"{} Recovered Transitions".format(transitions_count(Recovered_Signal)))
-# plt.plot(Time, Observed_Signal_copy - 1.5, 'o-', label=r"Observed")
-plt.legend(bbox_to_anchor=(0.85, 0.55), loc=2)
+
+# Measurement setup
+# t_QP_before = 10.0  # units ms
+# t_QP_after = 10.0  # units ms
+# t_meas = 0.1  # units ms
+# p_QP_before = 1.0 - exp(-t_meas / t_QP_before)
+# p_QP_after = 1.0 - exp(-t_meas / t_QP_after)
+
+# # Numbers for pomegranate which are fixed
+# p_e1 = 0.75
+# p_g0 = 0.95
+
+# t_QP_VBT = 10  # units ms
+# p_QP_VBT = 1.0 - exp(-t_meas / t_QP_VBT)
+
+length = 5000
+
+Hidden_Signal = generate_hidden_signal(length=length, charge_burst_time=length//2)
+Observed_Signal = hidden_to_observed_signal(Hidden_Signal)
+Recovered_Signal = observed_to_recovered_signal(Observed_Signal)
+
+fig = plt.figure(figsize=(12, 4))
+plt.plot(asarray(Hidden_Signal)+1.5, 'o-', label=r"{} Hidden Transitions".format(transitions_count(Hidden_Signal)))
+plt.plot(asarray(Recovered_Signal), 'o-', label=r"{} Recovered Transitions".format(transitions_count(Recovered_Signal)))
+# plt.plot(asarray(Observed_Signal) - 1.5, 'o-', label=r"Observed")
+plt.legend(bbox_to_anchor=(0.75, 0.58), loc=2)
 plt.show()
+
+"""
+TO DO
+1. Make it usable as other analysis function
+2. test its robustness against several fidelity and QP values
+"""
