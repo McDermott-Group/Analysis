703690653a2ac934ff554a74cc85213c4f9470bf
vincentliu66 <vincentchliu@outlook.com>
2020-04-23 15:39:54 -0500

    plot update.-LIU

--- projects/GapEngineering/Markov/analyze_stats.py
+++ projects/GapEngineering/Markov/analyze_stats.py
@@ -3,7 +3,7 @@ This is for test the analyze_QPTunneling_pomegranate.py robustness against noise
 """
 import numpy as np
 from analyze_QPTunneling_pomegranate import *
-from Learn_heatedmap import *
+from heatedmap import *
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,8 +13,8 @@ t_QP = 10.0  # units ms
 p_QP = 1.0 - exp(-t_meas/t_QP)
 
 
-p_0g_pool = np.linspace(100, 80, 11)/100
-p_1e_pool = np.linspace(60, 100, 11)/100
+p_0g_pool = np.linspace(100, 80, 2)/100
+p_1e_pool = np.linspace(60, 100, 2)/100
 error = np.zeros((len(p_0g_pool), len(p_1e_pool)))
 
 for r in range(len(p_0g_pool)):
@@ -23,7 +23,7 @@ for r in range(len(p_0g_pool)):
         tc_recovered = []
         readout_fidelity = [p_0g_pool[r], p_1e_pool[c]]
         print('read_fidelity=', readout_fidelity)
-        for i in range(10):
+        for i in range(2):
             # print('i=', i)
             Hidden_Signal = generate_hidden_signal(p_QP=[p_QP, p_QP])
             Observed_Signal = hidden_to_observed_signal(Hidden_Signal, readout_fidelity=readout_fidelity)
@@ -36,7 +36,7 @@ for r in range(len(p_0g_pool)):
         recovered_avg = average(tc_recovered)
         error[r][c] = ((recovered_avg-hidden_avg)/hidden_avg)*100
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(10, 10))
 
 im, cbar = heatmap(error, p_0g_pool, p_1e_pool, ax=ax,
                    cmap="YlGn", cbarlabel="error_2D [f_0g=95%, f_1e=75%]")
