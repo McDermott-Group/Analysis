260ae3f2b0d92a63f722294be269be712be5f111
vincentliu66 <vincentchliu@outlook.com>
2020-04-23 21:51:25 -0500

    1. VBT -> VTB 2. QP_Rate file, add the exponential expression instead of just 0.01 -LIU

--- projects/GapEngineering/Markov/analyze_QP_rate_stats.py
+++ projects/GapEngineering/Markov/analyze_QP_rate_stats.py
@@ -8,6 +8,7 @@ t_meas = 0.1  # units ms
 
 dummy_pool = np.linspace(100, 100, 1) / 100
 t_QP_pool = np.linspace(5, 20, 3)
+t_QP_VTB = 10
 error = np.zeros((len(dummy_pool), len(t_QP_pool)))
 
 for r in range(len(dummy_pool)):
@@ -15,10 +16,11 @@ for r in range(len(dummy_pool)):
         error_1D = []
         t_QP = t_QP_pool[c]  # units ms
         p_QP = 1.0 - exp(-t_meas/t_QP)
+        p_QP_VTB = 1.0 - exp(-t_meas/t_QP_VTB)
         for i in range(2):
             Hidden_Signal = generate_hidden_signal(p_QP=[p_QP, p_QP])
             Observed_Signal = hidden_to_observed_signal(Hidden_Signal)
-            Recovered_Signal = observed_to_recovered_signal(Observed_Signal)
+            Recovered_Signal = observed_to_recovered_signal(Observed_Signal, p_QP=p_QP_VTB)
             error_1D.append(((transitions_count(Recovered_Signal) - transitions_count(Hidden_Signal)) /
                              transitions_count(Hidden_Signal)) * 100)
         error[r][c] = average(error_1D).round(decimals=2)
