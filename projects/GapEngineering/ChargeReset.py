c514d6d14e268989572ca59c83db18957a26edfd
ADR3\lab_user <labrad@physics.wisc.edu>
2020-05-21 11:09:40 -0500

    Fix the initial guess issue by passing 5-10 guesses and choose the best fit. Need to find a way to make deal with the period.- LIU

--- projects/GapEngineering/ChargeReset.py
+++ projects/GapEngineering/ChargeReset.py
@@ -8,22 +8,33 @@ import matplotlib.pyplot as plt
 
 
 # np.random.seed(0)
-n = 1000
-x_off = 0.2
-noise_factor = 0.0
+n = 50
+x_off = round(np.random.uniform(-0.5, 0.5), 3)
+noise_factor = 0.1
+dispersion = 3.5
 x_data = np.linspace(-0.5, 0.5, num=n)
-y_data = 0.5 + 0.5 * cos(3.5 * pi * sin(2 * pi * (x_data-x_off)))
+y_data = 0.4 + 0.35 * cos(dispersion * pi * sin(2 * pi * (x_data-x_off)))
 y_data_noise = 1.0 * y_data + noise_factor * np.random.normal(size=n)
 
+initial_guess = np.linspace(-0.5, 0.5, num=6)
+covariance = float('inf')
+# print initial_guess
 def test_func(x, a):
-    return 0.5 + 0.5 * cos(3.5 * pi * sin(2 * pi * (x - a)))
+    return 0.5 + 0.5 * cos(dispersion * pi * sin(2 * pi * (x - a)))
 
-params, params_covariance = optimize.curve_fit(test_func, x_data, y_data_noise, p0=[0.1], method='trf')
+for ig in initial_guess:
+    params_curr, params_covariance_curr = optimize.curve_fit(test_func, x_data, y_data_noise, p0=[ig], method='trf')
+    # print ig, params_curr, params_covariance_curr
+    if params_covariance_curr[0] < covariance:
+        params = params_curr
+        params_covariance = params_covariance_curr
+        covariance = params_covariance_curr[0]
 
+
+print('x_off = ', round(x_off-0.5,3), round(x_off, 3), round(x_off+0.5, 3))
+print('fitted_off = ', round(params[0], 3))
 plt.figure(figsize=(6, 4))
-# plt.scatter(x_data, y_data, label='Data')
 plt.scatter(x_data, y_data_noise, label='Data_Noise')
 plt.plot(x_data, test_func(x_data, params[0]), label='Fitted function')
 plt.legend(loc='best')
-print(params)
 plt.show()
