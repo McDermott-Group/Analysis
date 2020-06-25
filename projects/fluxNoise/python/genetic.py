e0fc930960a1b1a7b5c81a54c2134e0e37aad240
Owen Rafferty <owenr@fferty.me>
2020-06-25 12:20:20 -0500

    T1 processing update

--- projects/fluxNoise/python/genetic.py
+++ projects/fluxNoise/python/genetic.py
@@ -1,20 +1,41 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import noiselib
-reload(noiselib)
+import pickle
 from noiselib import movingmean
+from scipy.optimize import curve_fit
 
 def T1_decay(dt):
     """dt should always be negative"""
     return 1.-np.exp(-T_rep*(np.arange(0,N)-dt)/tau)
 
-def gen_data():
+def fit_exp_dropoff(y, domain_length, origin):
+    f = np.max(y) - y
+
+    f = f[origin:(origin+domain_length)]
+
+    x = np.arange(0, domain_length)
+    popt, _ = curve_fit(lambda t,a,b: a*np.exp(-b*t),  x,  f)
+    a, b = popt[0], popt[1]
+
+    fit_x = np.zeros(N)
+    fit_x[0:origin], fit_x[(origin+domain_length):N] = np.nan, np.nan
+    fit_x[origin:(origin+domain_length)] = np.arange(0,domain_length)
+
+    fit_y = np.zeros(N)
+    fit_y[0:origin], fit_y[(origin+domain_length):N] = np.nan, np.nan
+    fit_y[origin:(origin+domain_length)] = -a*np.exp(-b*fit_x[origin:(origin+domain_length)])
+
+    return fit_x, fit_y
+
+def gen_data(T1_dropoff=True):
     T1 = T1_0 * np.ones((n,N))
+
     delta_t = np.random.random_sample(n) * dtrigbins
 
-    for i,dt in enumerate(delta_t):
-        b = int(np.ceil(dt))
-        T1[i,1000+b:] = T1[i,1000+b:] * T1_decay(dt - b)[:N-1000-b]
+    if T1_dropoff:
+        for i,dt in enumerate(delta_t):
+            b = int(np.ceil(dt))
+            T1[i,1000+b:] = T1[i,1000+b:] * T1_decay(dt - b)[:N-1000-b]
 
     r = np.random.random_sample((n,N))
     m = r < np.exp(-t_m/T1)
@@ -24,36 +45,23 @@ def gen_data():
 
     return m
 
-def gen_noise():
-    m = np.zeros((n,N))
-    mu = 0
-    sigma = 0.1
+def natural_selection(m, m_best, fitness_best):
+    y = np.mean(movingmean(m, 30), axis=0)
 
-    for r in range(n):
-        m[r] = mu + np.random.normal(mu, sigma, N)
-
-    return m
-
-def get_fitness(m, shifts, fitness_old):
-    y = np.mean(movingmean(m,30), axis=0)
-    idx_min = np.argmin(y)
-    #bound_l = int(idx_min + shifts.min())
-    #bound_u = int(idx_min + shifts.max())
-    bound_l = int(1000 + shifts.min())
-    bound_u = int(1000 + shifts.max())
-
-    #fitness = np.sum(y[bound_l:bound_u])
+    # invert y axis so larger sum -> higher fitness
     f = max(y) - y
-    fitness = np.trapz(f[bound_l:bound_u])
+
+    # sum +/- some amount around T1 @ 1000
+    fitness = np.sum(f[(1000 - 5):(1000 + 5)])
+    #fitness = np.trapz(f[990:1010])
     #fitness = abs(min(y) - np.median(y))
 
-    if fitness <= fitness_old:
-        more_fit = False
-        fitness = fitness_old
-    else:
-        more_fit = True
+    # return previous best if mutation does not increase fitness
+    if fitness <= fitness_best:
+        m = m_best
+        fitness = fitness_best
 
-    return y, fitness, more_fit
+    return m, fitness
 
 def shift_and_pad_array(arr, shift):
     ret = np.empty_like(arr)
@@ -79,9 +87,10 @@ def shift_rand_rows(m):
     shifts[rows_to_shift] = np.random.randint(-30, 30, n_to_shift)
 
     for r in rows_to_shift:
-        m[r] = shift_and_pad_array(m[r], int(shifts[r]))
+        #m[r] = shift_and_pad_array(m[r], int(shifts[r]))
+        m[r] = np.roll(m[r], int(shifts[r]))
 
-    return m, shifts
+    return m
 
 T1_0 = 40      # max T1
 tau = 5000     # QP relaxation time
@@ -91,21 +100,34 @@ t_m = 10       # time between X gate and measurement
 T_rep = 500    # Period of one rep
 dtrigbins = 30 # uncertainty in trigger timing
 
-fitness = 0
-m = gen_data()
-#m = gen_noise()
-for i in range(10):
+
+#m_best = gen_data()
+#m_best = gen_data(T1_dropoff=False)
+
+#f = open('T1_data_simulated.pckl', 'wb')
+#pickle.dump(m_best, f)
+#f.close()
+
+f = open('T1_data_simulated.pckl', 'rb')
+m_best = pickle.load(f)
+f.close()
+
+fitness_best = 0
+for i in range(0):
     print i
-    m, shifts = shift_rand_rows(m)
-    y_tmp, fitness, more_fit = get_fitness(m, shifts, fitness)
-    if more_fit == True:
-        y_best = y_tmp
+    m = shift_rand_rows(m_best)
+    m_best, fitness_best = natural_selection(m, m_best, fitness_best)
 
 x = np.arange(0, N)
+y = np.mean(movingmean(m_best, 30), axis=0)
 
 plt.figure()
-plt.plot(x, y_best)
-#plt.plot(x, np.median(y) * np.ones(N))
+plt.plot(x, y)
+
+#fit_domain_length = 500
+#origin = 1000
+#fit_x, fit_y = fit_exp_dropoff(y, fit_domain_length, origin)
+#plt.plot(origin + fit_x, fit_y + max(y[origin:(origin + fit_domain_length)]) )
 
 plt.draw()
 plt.pause(0.05)
